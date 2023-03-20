3c741e4325fd32d546a58c97c961193272447096
Artem Karpenko <dante120591@gmail.com>
2023-03-20 10:49:49 +0200

    Додає фавікон, опис, виправляє проблему прегнансі на айос

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,33 +24,43 @@ export const Header = ({ onMenuOpen }) => {
           </StyledLogoLink>
           <StyledLink to="/about" className={css.navItem}>
             About me
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/payment" className={css.navItem}>
             Payment
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/reviews" className={css.navItem}>
             Reviews
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/celebrity" className={css.navItem}>
             Celebrity
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/magazines" className={css.navItem}>
             Magazines
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/beauty" className={css.navItem}>
             Beauty
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/pregnancy" className={css.navItem}>
             Pregnancy
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/family" className={css.navItem}>
             Family
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/before-after" className={css.navItem}>
             Before/After
+            <span className={css.underline} />
           </StyledLink>
           <StyledLink to="/18+" className={css.navItem}>
             18+
+            <span className={css.underline} />
           </StyledLink>
         </div>
         <div className={css.burger} onClick={onMenuOpen}>
