85280d462ca337de8151cf7ffbb9fe93a7fbc860
Artem Karpenko <dante120591@gmail.com>
2023-03-20 13:12:05 +0200

    Додає для генерала, хедера і меню мобільну версію

--- src/components/General/General.jsx
+++ src/components/General/General.jsx
@@ -3,38 +3,41 @@ import css from './General.module.css';
 
 export function General() {
   return (
-    <div className={css.container}>
-      <Sidebar />
-      <div className={css.pageContainer}>
-        <div className={css.frameOne}>
-          <span className={css.titleOne}>This is retoucher</span>
-        </div>
-        <div className={css.frameTwo}>
-          <span className={css.titleTwo}>retouch</span>
-          <span className={css.titleThree}>photo</span>
-        </div>
-        <div className={css.frameThree}>
-          <span className={css.titleFour}>retoucheretoucher</span>
-        </div>
-        <div className={css.photoOne}>
-          <img
-            src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen1_2x_nbxing.jpg"
-            alt="women1"
-          />
-        </div>
-        <div className={css.photoTwo}>
-          <img
-            src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen2_2x_fncxhl.jpg"
-            alt="women2"
-          />
-        </div>
-        <div className={css.photoThree}>
-          <img
-            src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen3_2x_lco0bs.jpg"
-            alt="women3"
-          />
+    <>
+      <div className={css.container}>
+        <div className={css.pageContainer}>
+          <div className={css.frameOne}>
+            <span className={css.titleOne}>This is retoucher</span>
+          </div>
+          <div className={css.frameTwo}>
+            <span className={css.titleTwo}>retouch</span>
+            <span className={css.titleThree}>photo</span>
+          </div>
+          <div className={css.frameThree}>
+            <span className={css.titleFour}>retoucheretoucher</span>
+          </div>
+          <div className={css.photoOne}>
+            <img
+              src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen1_2x_nbxing.jpg"
+              alt="women1"
+            />
+          </div>
+          <div className={css.photoTwo}>
+            <img
+              src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen2_2x_fncxhl.jpg"
+              alt="women2"
+            />
+          </div>
+          <div className={css.photoThree}>
+            <img
+              src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678224123/this-is-retoucher/gen3_2x_lco0bs.jpg"
+              alt="women3"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <p className={css.footerEmail}>r.retouch.photo@gmail.com</p>
+      <Sidebar />
+    </>
   );
 }
