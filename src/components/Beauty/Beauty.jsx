80d7f19df03676c037f7430ad46bf88f7a68a286
Artem Karpenko <dante120591@gmail.com>
2023-03-25 19:16:26 +0200

    Додає відкривання фотографій до селебріті та б'юті, та анімаці

--- src/components/Beauty/Beauty.jsx
+++ src/components/Beauty/Beauty.jsx
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
+import { Lightbox } from 'components/Lightbox/Lightbox';
 import { Footer } from 'components/Footer/Footer';
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import css from './Beauty.module.css';
 
 export function Beauty() {
+  const [toggler, setToggler] = useState(false);
+  const [url, setUrl] = useState('');
+
   return (
     <>
+      <Lightbox toggler={toggler} url={url} />
       <div className={css.container}>
         <h1 className={css.pageTitle}>Beauty</h1>
         <div className={css.pageContainerUpper}>
@@ -13,6 +19,10 @@ export function Beauty() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678732241/this-is-retoucher/beauty1_ejgdfc.jpg"
               alt="woman face"
               className={css.photoOne}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameOne} />
             <span className={css.titleOne}>This is retoucher</span>
@@ -22,6 +32,10 @@ export function Beauty() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678732241/this-is-retoucher/beauty2_hphoyh.jpg"
               alt="woman ear"
               className={css.photoTwo}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameTwo} />
             <span className={css.titleTwo}>retouch</span>
@@ -33,6 +47,10 @@ export function Beauty() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678732242/this-is-retoucher/beauty3_tihscp.jpg"
               alt="woman flowers"
               className={css.photoThree}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameThree} />
             <span className={css.titleThreeDesktop}>retoucheretoucher</span>
@@ -43,6 +61,10 @@ export function Beauty() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678732241/this-is-retoucher/beauty4_mbkzrr.jpg"
               alt="woman makeup"
               className={css.photoFour}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameFour} />
             <span className={css.titleFour}>photo</span>
--- src/components/Celebrity/Celebrity.jsx
+++ src/components/Celebrity/Celebrity.jsx
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
+import { Lightbox } from 'components/Lightbox/Lightbox';
 import { Footer } from 'components/Footer/Footer';
 import { Sidebar } from 'components/Sidebar/Sidebar';
 import css from './Celebrity.module.css';
 
 export function Celebrity() {
+  const [toggler, setToggler] = useState(false);
+  const [url, setUrl] = useState('');
+
   return (
     <>
+      <Lightbox toggler={toggler} url={url} />
       <div className={css.container}>
         <h1 className={css.pageTitle}>Celebrity</h1>
         <div className={css.pageContainer}>
@@ -13,6 +19,10 @@ export function Celebrity() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678392548/this-is-retoucher/celeb1_qe2cag.jpg"
               alt="women sitting"
               className={css.photoOne}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameOne} />
             <span className={css.titleOne}>This is retoucher</span>
@@ -28,6 +38,10 @@ export function Celebrity() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678392548/this-is-retoucher/celeb2_xcxbhg.jpg"
               alt="woman blackhair"
               className={css.photoTwo}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameTwo} />
             <span className={css.titleTwo}>retouch</span>
@@ -41,6 +55,10 @@ export function Celebrity() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678392548/this-is-retoucher/celeb3_qtivqc.jpg"
               alt="50 cent"
               className={css.photoThree}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameThree} />
             <span className={css.titleThree}>retoucheretoucher</span>
@@ -53,6 +71,10 @@ export function Celebrity() {
               src="https://res.cloudinary.com/dmadhdzzm/image/upload/v1678392548/this-is-retoucher/celeb4_x5qd2u.jpg"
               alt="man and women"
               className={css.photoFour}
+              onClick={e => {
+                setToggler(!toggler);
+                setUrl(e.currentTarget.src);
+              }}
             />
             <div className={css.frameFour} />
             <span className={css.titleFour}>photo</span>
