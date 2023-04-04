1e26e5bb789ac1f017843a6bff6805ed35611ee7
Artem Karpenko <dante120591@gmail.com>
2023-04-04 20:04:41 +0300

    And again and again

--- src/i18n.js
+++ src/i18n.js
@@ -2,6 +2,10 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const options = {
+  order: ['localStorage', 'sessionStorage'],
+};
+
 i18n
   // pass the i18n instance to react-i18next.
   .use(initReactI18next)
@@ -13,6 +17,7 @@ i18n
   .init({
     debug: false,
     fallbackLng: 'en',
+    detection: options,
     // interpolation: {
     //   escapeValue: false, // not needed for react as it escapes by default
     // },
